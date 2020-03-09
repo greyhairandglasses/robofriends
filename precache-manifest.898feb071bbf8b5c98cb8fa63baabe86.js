96f45ea83cb6a45a6f762fbfa2a22527e78a913a
greyhairandglasses <dev@greyhairandglasses.com>
2020-03-09 15:18:00 +0000

    Updates

---
+++ precache-manifest.898feb071bbf8b5c98cb8fa63baabe86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fbf59f86f950787956eca3e0ec21bb",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9112295cfc6094072c8d",
+    "url": "/robofriends/static/css/main.164a22b7.chunk.css"
+  },
+  {
+    "revision": "e1a452469bc71b7403ab",
+    "url": "/robofriends/static/js/2.7ab315b8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.7ab315b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9112295cfc6094072c8d",
+    "url": "/robofriends/static/js/main.45d15b8f.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
